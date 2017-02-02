db9cf712bc2ee1f2c32a5227720d0e125f3f7c64
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-02-02 11:35:15 +0900

    Fix the build
    Signed-off-by: Kyle Gearhart <hgearhart@pivotal.io>

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -22,7 +22,6 @@ RSpec.describe "The FlightSchool application" do
 
   it "shows a list of common airports" do
     get "/"
-    expect(false).to be_true
     expect(last_response).to be_ok
     expect(last_response.body).to include("Current status of airports")
   end
